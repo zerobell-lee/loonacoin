a4c486bf5fcf3df67e6e2ddef5ded0b508c927db
zerobell-lee <bnb3456@gmail.com>
2019-04-05 01:17:50 +0900

    getting address and address balance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const EC = require('elliptic').ec,
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    _ = require('lodash');
 
 const ec = new EC('secp256k1');
 
@@ -11,6 +12,21 @@ const generatePrivateKey = () => {
     const privateKey = keyPair.getPrivate();
     return privateKey.toString(16);
 };
+
+const getPrivateFromWallet = () => {
+    const buffer = fs.readFileSync(privateKeyLocation, 'utf-8');
+    buffer.toString();
+}
+
+const getPublicFromWallet = () => {
+    const privateKey = getPrivateFromWallet();
+    const key = ec.keyFromPrivate(privateKey, 'hex');
+    return key.getPublic().encode('hex');
+};
+
+const getBalance = (address, uTxOuts) => {
+    return _(uTxOuts).filter(uTxOut => uTxOut.address === address).map(uTxOut => uTxOut.amount).sum();
+}
 
 const initWallet = () => {
     if (fs.existsSync(privateKeyLocation)) {
